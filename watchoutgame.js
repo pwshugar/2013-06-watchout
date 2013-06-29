2084c6db9de29e788cb84733b531b7dc56b99203
Hack Reactor Students <github@hackreactor.com>
2013-06-29 11:37:03 -0700

    started over with new html and js

--- watchoutgame.js
+++ watchoutgame.js
@@ -1,3 +1,5 @@
+(function(){
+
 var gameOptions = {
   height: 450,
   width: 700,
@@ -13,4 +15,34 @@ var gameStats = {
 var axes = {
   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+
+var gameBoard = d3.select('.container').append('svg:svg')
+                .attr('width', gameOptions.width)
+                .attr('height', gameOptions.height);
+
+var updateScore = function(){
+  d3.select('#current-score')
+      .text(gameStats.score.toString());
 };
+
+var updateBestScore = function(){
+  gameStats.bestScore = _.max([gameStats.bestScore, gameStats.score]);
+  return d3.select('#best-score').text(gameStats.bestScore.toString());
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}).call(this);
