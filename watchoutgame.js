b672a9914a82a2385b202b054484898c62f0f227
Hack Reactor Students <github@hackreactor.com>
2013-06-29 10:38:59 -0700

    created js and html files, starting to work on game

---
+++ watchoutgame.js
@@ -0,0 +1,16 @@
+var gameOptions = {
+  height: 450,
+  width: 700,
+  nEnemies: 30,
+  padding: 20
+};
+
+var gameStats = {
+  score: 0,
+  bestScore: 0
+};
+
+var axes = {
+  x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]),
+  y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height])
+};
