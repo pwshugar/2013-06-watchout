35e475314cd4d52b8114ab37cf39ed20d148fa09
Hack Reactor Students <github@hackreactor.com>
2013-06-29 19:56:10 -0700

    player drag is working

--- newjs.js
+++ newjs.js
@@ -1,11 +1,14 @@
-var body = d3.select("body").append("svg:svg");
+d3.select("#body").append("svg");
+var body = d3.select('svg');
+
 var data = new Array(30);
 
 var first = function(){
   body.selectAll("circle")
     .data(data)
-      .attr("cx", function() { return Math.floor(Math.random()*2000)})
-      .attr("cy", function() { return Math.floor(Math.random()*1000)})
+      .attr("cx", function() { return Math.floor(Math.random()*1960)})
+      .attr("cy", function() { return Math.floor(Math.random()*900)})
+      .attr("r", 10)
         .enter()
           .append("circle");
 };
@@ -14,9 +17,9 @@ var update = function() {
   body.selectAll("circle")
     .data(data)
        .transition()
-          .duration(2000)
-            .attr("cx", function() { return Math.floor(Math.random()*2000)})
-            .attr("cy", function() { return Math.floor(Math.random()*1000)})
+          .duration(3000)
+            .attr("cx", function() { return Math.floor(Math.random()*1960)})
+            .attr("cy", function() { return Math.floor(Math.random()*900)})
             .attr("r", 10);
 };
 
@@ -25,5 +28,21 @@ update();
 
 setInterval(function(){
   update();
-}, 2000);
+}, 3000);
 
+var player = body.append("circle")
+    .attr("cx", 1000)
+    .attr("cy", 500)
+    .attr("r", 20)
+    .attr("fill", "blue")
+      .call(d3.behavior.drag()
+        .on("drag", function(d) {moveObject.call(this);}));
+
+var moveObject = function(x,y){
+  var x = +d3.select(this).attr("cx");
+  var y = +d3.select(this).attr("cy");
+  x+=d3.event.dx;
+  y+=d3.event.dy;
+  d3.select(this).attr("cx",x);
+  d3.select(this).attr('cy', y);
+};
